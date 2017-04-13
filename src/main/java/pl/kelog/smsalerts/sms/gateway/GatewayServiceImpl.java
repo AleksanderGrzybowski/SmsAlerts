9f60274c32bb0631363c3352074a2b424fafe50b
Aleksander Grzybowski <kelog@orange.pl>
2017-04-13 15:56:18 +0200

    Integrate with GsmService API

--- src/main/java/pl/kelog/smsalerts/sms/gateway/GatewayServiceImpl.java
+++ src/main/java/pl/kelog/smsalerts/sms/gateway/GatewayServiceImpl.java
@@ -1,22 +1,48 @@
 package pl.kelog.smsalerts.sms.gateway;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import pl.kelog.smsalerts.sms.MessageDeliveryStatus;
 
 @Service
 class GatewayServiceImpl implements GatewayService {
     
+    private final String API_SEND_URL = "https://api.gsmservice.pl/v5/send.php";
+    private final String SMS_TYPE_ECO = "3";
+    
     private final String apiPassword;
     private final String apiUsername;
     
-    public GatewayServiceImpl(String apiUsername, String apiPassword ) {
+    public GatewayServiceImpl(String apiUsername, String apiPassword) {
         this.apiUsername = apiUsername;
         this.apiPassword = apiPassword;
     }
     
     @Override
     public MessageDeliveryStatus send(String recipient, String text) {
-        System.out.println("SENDING REAL");
-        return MessageDeliveryStatus.OK;
+        // http://stackoverflow.com/questions/38372422/how-to-post-form-data-with-spring-resttemplate
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        
+        MultiValueMap<String, String> bodyParams = new LinkedMultiValueMap<>();
+        bodyParams.add("login", apiUsername);
+        bodyParams.add("pass", apiPassword);
+        bodyParams.add("recipient", recipient);
+        bodyParams.add("message", text);
+        bodyParams.add("msg_type", SMS_TYPE_ECO);
+    
+        ResponseEntity<String> response = new RestTemplate().postForEntity(
+                API_SEND_URL,
+                new HttpEntity<>(bodyParams, headers),
+                String.class
+        );
+    
+        return response.getBody().startsWith("OK") ? MessageDeliveryStatus.OK : MessageDeliveryStatus.FAILED;
     }
 }
