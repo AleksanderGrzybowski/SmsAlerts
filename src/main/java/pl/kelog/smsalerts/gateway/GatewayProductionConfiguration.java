d84d9486c3836b22782d9ec69c3d282ab14d4a5c
Aleksander Grzybowski <kelog@orange.pl>
2017-04-25 20:50:00 +0200

    Use Spring configuration instead of raw environment vars

--- src/main/java/pl/kelog/smsalerts/gateway/GatewayProductionConfiguration.java
+++ src/main/java/pl/kelog/smsalerts/gateway/GatewayProductionConfiguration.java
@@ -1,5 +1,8 @@
 package pl.kelog.smsalerts.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,13 +11,18 @@ import org.springframework.context.annotation.Profile;
 @Profile("production")
 class GatewayProductionConfiguration {
     
+    private final Logger log = LoggerFactory.getLogger(GatewayProductionConfiguration.class);
+    
     @Bean
-    public GatewayService gatewayServiceImpl() {
-        String apiUsername = System.getenv("API_USERNAME");
-        String apiPassword = System.getenv("API_PASSWORD");
-        
+    public GatewayService gatewayServiceImpl(
+            @Value("${smsalerts.apiusername}") String apiUsername,
+            @Value("${smsalerts.apipassword}") String apiPassword
+    ) {
         if (apiUsername == null || apiPassword == null || apiUsername.length() == 0 || apiPassword.length() == 0) {
             throw new RuntimeException("Application running in production, but no credentials provided");
+        } else {
+            log.info("Provided api username (length "
+                    + apiUsername.length() + ") and password (length " + apiPassword.length() + ")");
         }
         
         return new GatewayServiceImpl(apiUsername, apiPassword);
--- src/main/java/pl/kelog/smsalerts/kspoller/KsPollerConfiguration.java
+++ src/main/java/pl/kelog/smsalerts/kspoller/KsPollerConfiguration.java
@@ -1,5 +1,8 @@
 package pl.kelog.smsalerts.kspoller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.kelog.smsalerts.ksdownloader.KsDownloaderService;
@@ -8,30 +11,33 @@ import pl.kelog.smsalerts.sms.MessageService;
 @Configuration
 public class KsPollerConfiguration {
     
+    private final Logger log = LoggerFactory.getLogger(KsPollerConfiguration.class);
+    
     @Bean
     public KsPollerService ksPollerService(
             KsInfoEntryRepository repository,
             KsDownloaderService downloaderService,
-            MessageService messageService
+            MessageService messageService,
+            @Value("${smsalerts.recipient}") String recipient
     ) {
-        String recipient = System.getenv("RECIPIENT");
         if (recipient == null || recipient.length() == 0) {
             throw new RuntimeException("No recipient phone number provided");
+        } else {
+            log.info("Messages will be sent to " + recipient);
         }
         
-        return new KsPollerServiceImpl(
-                repository,
-                downloaderService,
-                messageService,
-                recipient
-        );
+        return new KsPollerServiceImpl(repository, downloaderService, messageService, recipient);
     }
     
     @Bean
-    public KsPollerScheduledTask ksPollerScheduledTask(KsPollerService ksPollerService) {
-        String pattern = System.getenv("PATTERN");
+    public KsPollerScheduledTask ksPollerScheduledTask(
+            KsPollerService ksPollerService,
+            @Value("${smsalerts.pattern}") String pattern
+    ) {
         if (pattern == null) {
             throw new RuntimeException("No search pattern provided");
+        } else {
+            log.info("Search pattern: " + pattern);
         }
         
         return new KsPollerScheduledTask(ksPollerService, pattern);
