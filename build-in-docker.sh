b22f124ea33bda179680d92eff56f0f281ec5c7e
Aleksander Grzybowski <kelog@orange.pl>
2017-05-01 12:57:49 +0200

    Clean before building in Docker

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -v `pwd`:/SmsAlerts -u `stat -c "%u:%g" build.gradle` java:8-jdk /bin/bash -c "cd /SmsAlerts && ./gradlew test bootRepackage"
+docker run --rm -v `pwd`:/SmsAlerts -u `stat -c "%u:%g" build.gradle` java:8-jdk /bin/bash -c "cd /SmsAlerts && ./gradlew clean test bootRepackage"
