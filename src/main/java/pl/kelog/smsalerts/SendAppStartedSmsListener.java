ab39c1f12cf4049637fa41cb322732b570d090fb
Aleksander Grzybowski <kelog@orange.pl>
2018-10-07 08:31:02 +0200

    Send startup message through storing mechanism

--- src/main/java/pl/kelog/smsalerts/SendAppStartedSmsListener.java
+++ src/main/java/pl/kelog/smsalerts/SendAppStartedSmsListener.java
@@ -5,31 +5,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import pl.kelog.smsalerts.gateway.SmsGateway;
+import pl.kelog.smsalerts.sms.MessageService;
 
 @Component
 @Log
 class SendAppStartedSmsListener implements ApplicationListener<ApplicationReadyEvent> {
     
-    private final SmsGateway gateway;
+    private final MessageService messageService;
     
     private final String recipient;
     private final String commitId;
     
     public SendAppStartedSmsListener(
-            SmsGateway gateway,
+            MessageService messageService,
             @Value("${smsalerts.recipient}") String recipient,
             @Value("${git.commit.id}") String commitId
     ) {
-        this.gateway = gateway;
+        this.messageService = messageService;
         this.recipient = recipient;
         this.commitId = commitId;
     }
     
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        log.info("Sending startup message");
-        gateway.send(recipient, formatMessage());
+        log.info("Sending startup message...");
+        messageService.sendAndStore(recipient, formatMessage());
         log.info("Startup message sent.");
     }
     
